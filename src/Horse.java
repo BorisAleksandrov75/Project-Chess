383a1951a881a7212ec7dcdd1bd48921db872d75
Boris Aleksandrov <aleksandrov.bg78@gmail.com>
2022-03-29 21:34:30 +0900

    add class Horse

---
+++ src/Horse.java
@@ -0,0 +1,28 @@
+public class Horse extends ChessPiece {
+
+    public Horse(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toLine > 8 || toLine < 1 || toColumn > 7 || toColumn < 0) {
+            return false;
+        } else if (Math.abs(toLine - line) == 0 || Math.abs(toColumn - column) == 0 ) {
+            return false;
+        } else if (Math.abs(toLine - line) == 2 && Math.abs(toColumn - column) == 1 || Math.abs(toLine - line) == 1 && Math.abs(toColumn - column) == 2) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "H";
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        ChessBoard b = new ChessBoard("White");
+
+        b.printBoard();
+
+    }
+}

cf0e54ee0294a94e795f446f2291109332816cae
Boris Aleksandrov <aleksandrov.bg78@gmail.com>
2022-03-31 10:56:20 +0900

    add class Rook , Pawn (need Fix)

---
+++ src/Rook.java
@@ -0,0 +1,27 @@
+public class Rook extends ChessPiece{
+    public Rook(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (toLine > 7 || toLine < 0 || toColumn > 7 || toColumn < 0) {
+            return false;
+        } else if (Math.abs(toLine - line) == 0 && Math.abs(toColumn - column) == 0) {
+            return false;
+        } else if (Math.abs(toLine - line) >= 1 && Math.abs(toColumn - column) == 0 || Math.abs(toLine - line) == 0 && Math.abs(toColumn - column) >= 1 ) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "R";
+    }
+}

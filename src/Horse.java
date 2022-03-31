81fb816dee785e8ca87f2b43f3581f9dfd7866ae
Boris Aleksandrov <aleksandrov.bg78@gmail.com>
2022-03-31 10:34:13 +0900

    add class Bishop , Pawn (need Fix)

--- src/Horse.java
+++ src/Horse.java
@@ -11,7 +11,7 @@ public class Horse extends ChessPiece {
 
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
-        if (toLine > 8 || toLine < 1 || toColumn > 7 || toColumn < 0) {
+        if (toLine > 7 || toLine < 0 || toColumn > 7 || toColumn < 0) {
             return false;
         } else if (Math.abs(toLine - line) == 0 || Math.abs(toColumn - column) == 0 ) {
             return false;
--- src/Pawn.java
+++ src/Pawn.java
@@ -14,15 +14,17 @@ public class Pawn extends ChessPiece{
     @Override
     public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
 
-        if (toLine > 8 || toLine < 1 || toColumn > 7 || toColumn < 0) {
+        if (toLine > 7 || toLine < 0 || toColumn > 7 || toColumn < 0) {
             return false;
-        } else if (Math.abs(toColumn - column) == 0 && Math.abs(toLine - line) == 0) {
+        } else if (Math.abs(toColumn - column) == 0 || Math.abs(toLine - line) == 0 || Math.abs(line - toLine) == 0 )  {
             return false;
-        }  else if (color.equals("White") && column == 1 && toColumn - column == 2 && Math.abs(toLine - line)  == 0) {
+        }  else if (color.equals("White") && line == 1 && (toLine - line) == 2 && Math.abs(toColumn - column)  == 0) {
             return true;
-        } else if (color.equals("Black") && column == 6 && column - toColumn  == 2 && Math.abs(toLine - line)  == 0 ) {
+        } else if (color.equals("White") && (toLine - line) == 1 && Math.abs(toColumn - column)  == 0 ) {
             return true;
-        } else if (toColumn - column == 1 && Math.abs(toLine - line)  == 0 || column - toColumn == 1 && Math.abs(toLine - line) == 0 ) {
+        } else if (color.equals("Black") && line == 7 && (line - toLine)  == 2 && Math.abs(toColumn - column)  == 0 ) {
+            return true;
+        } else if (color.equals("Black") && (line - toLine) == 1 && Math.abs(toColumn - column) == 0) {
             return true;
         }
         return false;
